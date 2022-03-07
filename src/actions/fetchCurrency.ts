f89fbb95c2d44666f963ee852e8fddaf86a19592
Sean Smith <sws@Seans-MacBook.lan>
2022-03-07 14:18:50 -0800

    updated api

--- src/actions/fetchCurrency.ts
+++ src/actions/fetchCurrency.ts
@@ -1,6 +1,6 @@
 const fetchCurrency = async () => {
   const { data } = await fetch(
-    "https://freecurrencyapi.net/api/v2/latest?apikey=a36f3760-9424-11ec-8c67-ddb97e380620"
+    "https://currencyapi.com/api/v2/latest?apikey=a36f3760-9424-11ec-8c67-ddb97e380620"
   ).then((r) => r.json());
   return data;
 };
